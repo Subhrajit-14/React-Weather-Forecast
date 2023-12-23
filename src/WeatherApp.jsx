bc16572fbc81d416cdbc8f27455be9f7d126ebce
Subhrajit-14 <subhrajitswain1678@gmail.com>
2023-12-23 20:19:47 +0530

    Add initial files

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,21 @@
+import SearchBox from "./SearchBox";
+import InfoBox from "./InfoBox";
+import './WeatherApp.css';
+import { useState } from "react";
+import '@fontsource/roboto/500.css';
+
+export default function WeatherApp() {
+    const [weatherInfo, setWeatherInfo] = useState({
+        
+    });
+    let infoWeather = (infoWeather) => {
+        setWeatherInfo(infoWeather);
+    }
+	return (
+		<div className="WeatherApp">
+			<h1 className="weatherHead">Weather App</h1>
+			<SearchBox infoWeather={infoWeather} />
+			<InfoBox info={weatherInfo} />
+		</div>
+	);
+}
